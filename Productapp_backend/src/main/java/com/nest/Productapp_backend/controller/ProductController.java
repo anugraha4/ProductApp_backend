8753c3eb14b678c6b7e75fe44030c13cff415fe1
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-12-05 18:13:44 +0530

    added

--- Productapp_backend/src/main/java/com/nest/Productapp_backend/controller/ProductController.java
+++ Productapp_backend/src/main/java/com/nest/Productapp_backend/controller/ProductController.java
@@ -1,4 +1,13 @@
 package com.nest.Productapp_backend.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+
+    @PostMapping("/")
+    public String ProductAdd(){
+        return "product added";
+    }
 }
